a4c7e43dfa2685b1f7c576386a2272871a30a40d
nagarajuchary <nagarajuvita13@gmail.com>
2024-04-01 11:32:36 +0530

    genetaredAdminModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminNestModule } from './admin-nest/admin-nest.module';
 
 @Module({
-  imports: [],
+  imports: [AdminNestModule],
   controllers: [AppController],
   providers: [AppService],
 })
